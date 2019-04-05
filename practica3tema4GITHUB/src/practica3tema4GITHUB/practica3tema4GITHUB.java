0842abd5217f2ce9f1568fc24214e38374dc6877
jorge <jorge@10.0.2.15>
2019-04-05 18:20:46 +0200

    practica /de+
    + vmnz<aQz

---
+++ practica3tema4GITHUB/src/practica3tema4GITHUB/practica3tema4GITHUB.java
@@ -0,0 +1,11 @@
+package practica3tema4GITHUB;
+
+public class practica3tema4GITHUB {
+
+	public static void main(String[] args) {
+		System.out.println("hola");
+		System.out.println("ejercicio 3 tema 4 de entornos ");
+
+	}
+
+}
