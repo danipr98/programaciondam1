8a9d9f365e1d20fd772d243b5ec3212d547c8fde
jorge <jorge@10.0.2.15>
2019-04-05 18:29:18 +0200

    segundo cambio en el repositorio local

--- practica3tema4GITHUB/src/practica3tema4GITHUB/practica3tema4GITHUB.java
+++ practica3tema4GITHUB/src/practica3tema4GITHUB/practica3tema4GITHUB.java
@@ -5,7 +5,7 @@ public class practica3tema4GITHUB {
 	public static void main(String[] args) {
 		System.out.println("hola");
 		System.out.println("ejercicio 3 tema 4 de entornos ");
-
+		System.out.println("segundo cambio ");
 	}
 
 }
